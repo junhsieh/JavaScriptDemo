597b7ee11383d0cdf11432b8520c75f2e8037c47
Jun Hsieh <jun.github@ijun.org>
2019-02-27 05:14:00 +0000

    update

--- vue/exp2/src/components/Dashboard.vue
+++ vue/exp2/src/components/Dashboard.vue
@@ -1,16 +1,15 @@
 <template>
     <div>
-        <Icon type="md-bug" size="30" />
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'Dashboard',
-        props: {
-            msg: String
-        }
-    }
+export default {
+  name: 'Dashboard',
+  props: {
+    msg: String
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
