1a86f256bbde1f41b7cdae44b275d3079703c32f
Jun Hsieh <jun.github@ijun.org>
2019-02-27 04:44:14 +0000

    update

--- vue/exp2/src/views/Dashboard.vue
+++ vue/exp2/src/views/Dashboard.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <h1>This is dashboard</h1>
+    <h1>Dashboard</h1>
+    <Dashboard msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Dashboard from '@/components/Dashboard.vue'
+
+export default {
+  name: 'SalesOrder',
+  components: {
+    Dashboard,
+  }
+}
+</script>
