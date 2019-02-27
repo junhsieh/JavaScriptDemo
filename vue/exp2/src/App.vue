0f111fe952e04605ad70a0c6c8a4ee81d0e81f4e
Jun Hsieh <jun.github@ijun.org>
2019-02-27 05:25:39 +0000

    update

--- vue/exp2/src/App.vue
+++ vue/exp2/src/App.vue
@@ -19,7 +19,7 @@
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
+  padding: 10px;
 }
 
 #nav a {
--- vue/exp2/src/views/Dashboard.vue
+++ vue/exp2/src/views/Dashboard.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <h1>Dashboard <Icon type="md-bug" size="30" /></h1>
-    <Dashboard msg="Welcome to Your Vue.js App"/>
     <GridLayout
             :layout.sync="layout"
             :col-num="12"
@@ -31,26 +30,12 @@
 import VueGridLayout from 'vue-grid-layout';
 
 	var testLayout = [
-	    {"x":0,"y":0,"w":2,"h":2,"i":"0"},
-	    {"x":2,"y":0,"w":2,"h":4,"i":"1"},
-	    {"x":4,"y":0,"w":2,"h":5,"i":"2"},
-	    {"x":6,"y":0,"w":2,"h":3,"i":"3"},
-	    {"x":8,"y":0,"w":2,"h":3,"i":"4"},
-	    {"x":10,"y":0,"w":2,"h":3,"i":"5"},
-	    {"x":0,"y":5,"w":2,"h":5,"i":"6"},
-	    {"x":2,"y":5,"w":2,"h":5,"i":"7"},
-	    {"x":4,"y":5,"w":2,"h":5,"i":"8"},
-	    {"x":6,"y":4,"w":2,"h":4,"i":"9"},
-	    {"x":8,"y":4,"w":2,"h":4,"i":"10"},
-	    {"x":10,"y":4,"w":2,"h":4,"i":"11"},
-	    {"x":0,"y":10,"w":2,"h":5,"i":"12"},
-	    {"x":2,"y":10,"w":2,"h":5,"i":"13"},
-	    {"x":4,"y":8,"w":2,"h":4,"i":"14"},
-	    {"x":6,"y":8,"w":2,"h":4,"i":"15"},
-	    {"x":8,"y":10,"w":2,"h":5,"i":"16"},
-	    {"x":10,"y":4,"w":2,"h":2,"i":"17"},
-	    {"x":0,"y":9,"w":2,"h":3,"i":"18"},
-	    {"x":2,"y":6,"w":2,"h":2,"i":"19"}
+	    {"x":0,"y":0,"w":3,"h":6,"i":"0"},
+	    {"x":3,"y":0,"w":3,"h":6,"i":"1"},
+	    {"x":6,"y":0,"w":3,"h":6,"i":"2"},
+	    {"x":0,"y":6,"w":3,"h":6,"i":"3"},
+	    {"x":3,"y":6,"w":3,"h":6,"i":"4"},
+	    {"x":6,"y":6,"w":3,"h":6,"i":"5"},
 	];
 
 export default {
@@ -66,3 +51,11 @@ export default {
   }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.vue-grid-item {
+	background: #bcde94;
+}
+</style>
+
